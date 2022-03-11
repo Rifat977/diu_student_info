df70ea9bcc3f74f72f4375394e8ba4a615a822c1
rifat977 <itscrifat5147@gmail.com>
2022-03-11 19:27:33 +0600

    new

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const conrs = require('cors');
 
 const app = express()
+
+app.use(cors({
+	origin: "https://diu-api.herokuapp.com/"
+}))
 
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
