7e77413d56df00b5d084e7fae16f5aebf7ad71af
rifat977 <itscrifat5147@gmail.com>
2022-03-11 17:34:58 +0600

    new

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+const cors = require('cors')
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(cors()).mount('#app')
