3021dfdd4a7a778b1f085df736bb2d8b2ad0b7e3
rifat977 <itscrifat5147@gmail.com>
2022-03-15 22:45:22 +0600

    Nce

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    headers: {
+      "Allow-Access-Control-Origin" : "*"
+    }
+  },
   transpileDependencies: true
 })
